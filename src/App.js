e24c2395225964e939fe46879dea5d760e340202
shsandadi <harsha.sandadi@gmail.com>
2021-12-16 18:57:00 +0530

    Wrapped all components into  GlobalProvider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Balance from "./components/Balance";
 import Header from "./components/Header";
 import IncomeExpense from "./components/IncomeExpense";
 import TransactionList from "./components/TransactionList";
-
+import { GlobalProvider } from "./context/GlobalState";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Balance />
-      <IncomeExpense />
-      <TransactionList />
+      <GlobalProvider>
+        <Header />
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+      </GlobalProvider>
     </div>
   );
 }
